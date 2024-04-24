3b4b1daba3a06f6fd83f1b9e67c8121747021170
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-04-24 00:55:10 -0300

    Comment GitHub Desktop from installation

--- post-instalation.sh
+++ post-instalation.sh
@@ -11,8 +11,8 @@ sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-bui
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 
 # Feed github desktop package
-wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+# wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+# sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 
 # Adding video drivers repositories
 sudo add-apt-repository ppa:kisak/kisak-mesa
@@ -26,7 +26,7 @@ nvm install $NODE_VERSION
 sudo apt update && sudo apt upgrade -y
 
 # Apt packages
-sudo apt install baobab lutris sqlite pip vlc git github-desktop libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 nodejs krita gimp htop neofetch net-tools ssh python3-tk -y
+sudo apt install baobab lutris sqlite pip vlc git libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 krita gimp htop neofetch net-tools ssh python3-tk -y
 sudo apt install --install-recommends winehq-devel -y
 
 # Python modules
