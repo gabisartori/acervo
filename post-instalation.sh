5f16d926ac1628ff0c831c1f8142e25f4c5ab858
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-04-24 01:51:07 -0300

    Add .deb packages to installation, rezando muito pra dar certo isso

--- post-instalation.sh
+++ post-instalation.sh
@@ -29,6 +29,33 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install baobab lutris sqlite pip vlc git libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 krita gimp htop neofetch net-tools ssh python3-tk -y
 sudo apt install --install-recommends winehq-devel -y
 
+# Deb packages
+## Discord
+curl -s -L --output /tmp/discord.deb "https://discord.com/api/download?platform=linux&format=deb"
+sudo dpkg -i /tmp/discord.deb
+## Minecraft
+curl -s -L --output /tmp/minecraft.deb "https://launcher.mojang.com/download/Minecraft.deb"
+sudo dpkg -i /tmp/minecraft.deb
+## Steam
+curl -s -L --output /tmp/steam.deb "https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb"
+sudo dpkg -i /tmp/steam.deb
+## Visual Studio Code
+curl -s -L --output /tmp/vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo dpkg -i /tmp/vscode.deb
+## Libre Office
+mkdir /tmp/libreoffice_deb
+### Libre Office
+curl -s -L --output /tmp/libreoffice_deb.tar.gz "https://download.documentfoundation.org/libreoffice/stable/7.6.6/deb/x86_64/LibreOffice_7.6.6_Linux_x86-64_deb.tar.gz"
+tar -xvf /tmp/libreoffice_deb.tar.gz -C /tmp/libreoffice_deb
+### Translation pt-br
+curl -s -L --output /tmp/libreoffice_pt_br_deb.tar.gz "https://download.documentfoundation.org/libreoffice/stable/7.6.6/deb/x86_64/LibreOffice_7.6.6_Linux_x86-64_deb_langpack_pt-BR.tar.gz"
+tar -xvf /tmp/libreoffice_pt_br_deb.tar.gz -C /tmp/libreoffice_deb
+### Help Pack pt-br
+curl -s -L --output /tmp/libreoffice_helppack_pt_br_deb.tar.gz "https://download.documentfoundation.org/libreoffice/stable/7.6.6/deb/x86_64/LibreOffice_7.6.6_Linux_x86-64_deb_helppack_pt-BR.tar.gz"
+tar -xvf /tmp/libreoffice_helppack_pt_br_deb.tar.gz -C /tmp/libreoffice_deb
+
+sudo dpkg --recursive -i /tmp/libreoffice_deb
+
 # Python modules
 pip3 install numpy pandas tensorflow scikit-learn matplotlib
 
