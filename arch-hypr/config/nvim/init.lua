250ca3e082f12a36dff943691ce5fc4f1cc64ae3
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2025-02-18 03:52:50 -0300

    Migrate vim init file to lua

---
+++ arch-hypr/config/nvim/init.lua
@@ -0,0 +1,18 @@
+-- Neovim settings
+-- Leave tabstop at deafult value of 8 spaces
+-- This is because I don't want to use tabs, so they should stick out
+-- vim.opt.tabstop = "8"
+-- Automatic identation for when breaking lines;
+-- vim.opt.shiftwidth = "4"
+-- Syntax highlighting
+vim.opt.syntax = "on"
+-- Show line numbers
+vim.opt.number = true
+-- Highlight the current line the cursor is on
+vim.opt.cursorline = true
+-- Set clipboard to be the same as the system instead of its own
+vim.opt.clipboard = "unnamedplus"
+-- Plugins
+require("config.lazy")
+
+
