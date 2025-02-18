8bbd4dcb5bed76d9ca55c2ad4471e147a265b100
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2025-02-18 04:47:18 -0300

    Add lazy nvim plugin manager to nvim and install tree plugin

--- arch-hypr/config/nvim/init.lua
+++ arch-hypr/config/nvim/init.lua
@@ -1,9 +1,11 @@
 -- Neovim settings
 -- Leave tabstop at deafult value of 8 spaces
 -- This is because I don't want to use tabs, so they should stick out
--- vim.opt.tabstop = "8"
+-- vim.opt.tabstop = 8
 -- Automatic identation for when breaking lines;
--- vim.opt.shiftwidth = "4"
+vim.opt.shiftwidth = 4
+-- Indent using spaces instead of tabs
+vim.opt.expandtab = true
 -- Syntax highlighting
 vim.opt.syntax = "on"
 -- Show line numbers
@@ -12,7 +14,13 @@ vim.opt.number = true
 vim.opt.cursorline = true
 -- Set clipboard to be the same as the system instead of its own
 vim.opt.clipboard = "unnamedplus"
--- Plugins
+
+
+-- PLUGINS
+-- lazy.nvim plugin manager
 require("config.lazy")
+
+-- Disable hererocks
+-- opts.rocks.hererocks = false
 
 
