bf448f19e06777b9af1aac881d0a34e8a984e52f
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2025-03-06 19:42:18 -0300

    Setup man and remove sudos from inside setup.sh

--- arch-hypr/setup.sh
+++ arch-hypr/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --needed git base-devel
+pacman -S --needed git base-devel
 
 # Install yay
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
@@ -9,6 +9,9 @@ cd .. && rm -rf yay-bin
 # Install packages
 yay -S --needed - < packages.list
 
-
 # Config files
 ln -sf "$(pwd)"/config/* ~/.config/
+
+# Setup man-pages
+mandb
+
