bf729a80d525a33c7c22d35b467137ecb6b36ffa
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2025-07-17 19:31:54 -0300

    Add bashrc

--- arch-hypr/setup.sh
+++ arch-hypr/setup.sh
@@ -11,8 +11,21 @@ yay -S --needed - < packages.list
 
 # Config files
 ln -sf "$(pwd)"/config/* ~/.config/
+
+ln -sf "$(pwd)"/scripts/bashrc ~/.bashrc
 ln -sf "$(pwd)"/scripts/bash_aliases ~/.bash_aliases
+ln -sf "$(pwd)"/scripts/bash_venv ~/.bash_venv
+ln -sf "$(pwd)"/scripts/bash_autocomplete ~/.bash_autocomplete
 
 # Setup man-pages
 mandb
+
+# TODO: Setup sddm (Display manager aka Login manager)
+
+# TODO: set kwallet password to be empty or to autounlock on login
+# the empty option is what has worked until now
+
+# Enable services
+sudo systemctl enable sddm
+sudo systemctl enable bluetooth
 
