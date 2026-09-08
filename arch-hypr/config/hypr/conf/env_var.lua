88ff989503e72f795b641dbc79244aa0beb49722
gabisartori <gabriel.sartorirangel@gmail.com>
2026-09-08 16:37:37 -0300

    Move text editor and ibus related environment variables from bashrc to hyprland setup

--- arch-hypr/config/hypr/conf/env_var.lua
+++ arch-hypr/config/hypr/conf/env_var.lua
@@ -11,5 +11,13 @@ hl.env("XDG_CONFIG_HOME", homeDir .. "/.config")
 hl.env("XDG_DATA_HOME", homeDir .. "/.local/share")
 hl.env("XDG_CACHE_HOME", homeDir .. "/.cache")
 
+-- Setup Ibus
+-- TODO: I don't think this is actually working rn
+-- I should also replace ibus with fcitx
+hl.env("GTK_IM_MODULE", "ibus")
+hl.env("QT_IM_MODULE", "ibus")
+hl.env("SDL_IM_MODULE", "ibus")
+hl.env("XMODIFIERS", "@im=ibus")
+
 -- Personal variables
 hl.env("EDITOR", "nvim")
